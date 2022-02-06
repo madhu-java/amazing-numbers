20681634cb69a46c3320410329dbac3d8f4d7d3e
Madhavi <madhavirontala4@gmail.com>
2022-02-06 19:21:26 +1100

    added palindrome test

--- Amazing Numbers/task/src/numbers/Main.java
+++ Amazing Numbers/task/src/numbers/Main.java
@@ -6,39 +6,88 @@ public class Main {
     public static void main(String[] args) {
 //        write your code here
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a natural number:");
-        int number = scanner.nextInt();
-        if(number<=0){
-            System.out.println("This number is not natural!");
-        } else{
-            if(number%2==0){
-                System.out.println("This number is Even.");
-            }else{
-                System.out.println("This number is Odd.");
+        System.out.println("Welcome to Amazing Numbers!\n" +
+               "Supported requests:\n" +
+                "- enter a natural number to know its properties;\n" +
+                "- enter 0 to exit.\n");
+        boolean exit= false;
+        while(!exit){
+
+
+        System.out.println("Enter a request:");
+        long number = scanner.nextLong();
+        boolean even=false;
+        //boolean odd=false;
+        boolean buzz=false;
+        boolean duck =false;
+        boolean isPalindrome =false;
+         boolean isNotNatural= number<=0?true:false;
+        if(isNotNatural){
+            if(number==0){
+                System.out.println("Goodbye!");
+                exit=true;
+            }else {
+                System.out.println("The first parameter should be a natural number or zero.");
+                continue;
             }
+        } else  {
+            even = number % 2 == 0;
+            buzz = number % 7 == 0 || number % 10 == 7;
+            String numberString = "" + number;
+            if (numberString.length()>1 && numberString.substring(1).contains("0")) {
+                duck = true;
+            }
+            long pal = getPal(number);
+            isPalindrome = pal==number;
+            System.out.println("Properties of " + number + "\n" +
+                    "        even: " + even + "\n" +
+                    "         odd: " + !even + "\n" +
+                    "        buzz: " + buzz + "\n" +
+                    "        duck: " + duck +"\n"+
+                    "  palindromic: " + isPalindrome);
+        }
+//            if(number%2==0){
+//                even=true;
+//               // System.out.println("This number is Even.");
+//            }else{
+//               // System.out.println("This number is Odd.");
+//                odd=true;
+//            }
 
-                if(number%7==0 || number%10==7){
-                    System.out.println("It is a Buzz number.");
 
-                }else{
-                    System.out.println("It is not a Buzz number.");
-                }
-            System.out.println("Explanaton:");
-                if(number%7==0) {
-                    if (number % 10 == 7) {
-                        System.out.println(number + "  is divisible by 7 and ends with 7.");
-                    } else {
-
-                        System.out.println(number + "is divisible by 7.");
-                    }
-                }else if (number % 10 == 7) {
-                    System.out.println(number + " ends with 7.");
-                } else
-                {
-                    System.out.println(number+" is neither divisible by 7 nor does it end with 7.");
-
-                    }
+//                   // System.out.println("It is a Buzz number.");
+//                    buzz=true;
+//
+//                }else{
+//                   // System.out.println("It is not a Buzz number.");
+//                    buzz=false;
+//                }
+//            System.out.println("Explanaton:");
+//                if(number%7==0) {
+//                    if (number % 10 == 7) {
+//                        System.out.println(number + "  is divisible by 7 and ends with 7.");
+//                    } else {
+//
+//                        System.out.println(number + "is divisible by 7.");
+//                    }
+//                }else if (number % 10 == 7) {
+//                    System.out.println(number + " ends with 7.");
+//                } else
+//                {
+//                    System.out.println(number+" is neither divisible by 7 nor does it end with 7.");
+//
+//                    }
         }
 
+    }
+
+    private static long getPal(long number) {
+        long pal=0;
+        for(long num = number; num>0;){
+            System.out.println("number:"+num);
+            pal = (pal*10) + num%10;
+            num /= 10;
+        }
+        return pal;
     }
 }
